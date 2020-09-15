49812fedd8d171543b467449cffe182dc302dc2d
Cassandra <cassandra+git@services.beelen.one>
2020-09-15 16:30:32 +0200

    Only decrypt .crypt files

--- ci/decrypt.py
+++ ci/decrypt.py
@@ -3,7 +3,6 @@
 import shutil
 import os
 import pathlib
-import tempfile
 
 from .crypt import decrypt, vault_regex
 
@@ -16,7 +15,7 @@ def command(args):
 
 def decrypt_path(path: pathlib.Path):
     for root, _, files in os.walk(path):
-        for file in files:
+        for file in (x for x in files if x.endswith('.crypt')):
             path = pathlib.Path(root, file)
             decrypt_file(path)
 
@@ -34,10 +33,8 @@ def decrypt_file(path: pathlib.Path):
         newdata = newdata.replace(match.group(), secret, 1)
 
     if newdata != original:
-        with tempfile.TemporaryFile() as tmp:
-            newpath = path if path.suffix != '.crypt' else path.with_suffix('')
-            shutil.copystat(path, tmp.name)
-            with newpath.open('w') as f:
-                f.write(newdata)
-            shutil.copystat(tmp.name, newpath)
-            print(newpath)
+        newpath = path.with_suffix('')
+        with newpath.open('w') as f:
+            f.write(newdata)
+        shutil.copystat(path, newpath)
+        print(newpath)
