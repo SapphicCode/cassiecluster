66f991aa3ce8f7828442f6d08b9ca2fa0d2ca33f
Cassandra <cassandra+git@services.beelen.one>
2020-10-06 14:31:18 +0200

    Add basic Terraform config

---
+++ terraform/aws_buckets.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+variable "static_sites" {
+  type = list(string)
+  default = [
+    "meta.qcx.io",
+    "cassandra.beelen.one"
+  ]
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = var.static_sites[count.index]
+
+  website {
+    error_document = "404.html"
+    index_document = "index.html"
+  }
+
+  count = length(var.static_sites)
+}
+
+resource "aws_s3_bucket_policy" "static_site" {
+  bucket = var.static_sites[count.index]
+  policy = jsonencode(yamldecode(templatefile("../configs/aws/policies/s3-cloudflare.yml", { bucket = var.static_sites[count.index] })))
+
+  count = length(var.static_sites)
+}
