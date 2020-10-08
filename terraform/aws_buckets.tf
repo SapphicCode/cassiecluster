cb044ab9a9553b3aeab9b9b3b94716525f7db2ae
Cassandra <cassandra+git@services.beelen.one>
2020-10-08 14:53:45 +0200

    Close #1

--- terraform/aws_buckets.tf
+++ terraform/aws_buckets.tf
@@ -1,30 +1,28 @@
-variable "static_sites" {
-  type = list(string)
-  default = [
+locals {
+  static_sites = toset([
     "meta.qcx.io",
     "cassandra.beelen.one"
-  ]
+  ])
 }
 
 resource "aws_s3_bucket" "static_site" {
-  bucket = var.static_sites[count.index]
-
+  bucket = each.key
   website {
     error_document = "404.html"
     index_document = "index.html"
   }
 
-  count = length(var.static_sites)
+  for_each = local.static_sites
 }
 
 resource "aws_s3_bucket_policy" "static_site" {
-  bucket = aws_s3_bucket.static_site[count.index].bucket
+  bucket = each.value.bucket
   policy = jsonencode(yamldecode(templatefile(
     "../configs/aws/policies/s3-cloudflare.yml",
-    { bucket = aws_s3_bucket.static_site[count.index].bucket }
+    { bucket = each.value.bucket }
   )))
 
-  count = length(aws_s3_bucket.static_site)
+  for_each = aws_s3_bucket.static_site
 }
 
 resource "aws_s3_bucket" "archive" {
