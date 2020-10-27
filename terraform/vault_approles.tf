956c88fe60331aaf5c965a182a037b90f34487fb
Cassandra <cassandra+git@services.beelen.one>
2020-10-27 13:40:24 +0100

    Reference role resources in approle

--- terraform/vault_approles.tf
+++ terraform/vault_approles.tf
@@ -2,14 +2,18 @@
 resource "vault_approle_auth_backend_role" "terraform" {
   role_name = "terraform"
 
-  token_policies = ["ci"]
-  token_ttl      = 30 * 60
+  token_policies = [
+    vault_policy.ci.name,
+  ]
+  token_ttl = 30 * 60
 }
 
 // Personal admin roles
 resource "vault_approle_auth_backend_role" "iphone" {
   role_name = "iphone"
 
-  token_policies = ["admin"]
-  token_ttl      = 60
+  token_policies = [
+    vault_policy.admin.name,
+  ]
+  token_ttl = 60
 }
