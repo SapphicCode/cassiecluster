8899b3774952b23173cdd65c888f15bfdb27d961
Cassandra <cassandra+git@services.beelen.one>
2020-10-27 13:37:12 +0100

    Add personal role to Vault

--- terraform/vault_approles.tf
+++ terraform/vault_approles.tf
@@ -1,6 +1,15 @@
+// CI tooling
 resource "vault_approle_auth_backend_role" "terraform" {
   role_name = "terraform"
 
   token_policies = ["ci"]
   token_ttl      = 30 * 60
+}
+
+// Personal admin roles
+resource "vault_approle_auth_backend_role" "iphone" {
+  role_name = "iphone"
+
+  token_policies = ["admin"]
+  token_ttl      = 60
 }
