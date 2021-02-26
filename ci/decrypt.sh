6e9cc114a1104d3065c7fa72eca2531ea0e2208b
Cassandra <cassandra+git@beelen.one>
2021-02-26 16:48:28 +0100

    Add CI scripts

---
+++ ci/decrypt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+shopt -s globstar
+set -euxo pipefail
+
+for encrypted_file in **/*.transit; do
+  plain_file=${encrypted_file%.*}
+  vault write -field=plaintext transit/cluster/decrypt/automata "ciphertext=@${encrypted_file}" | base64 -d > "${plain_file}" || rm "${plain_file}" && false
+  touch -r "${encrypted_file}" "${plain_file}"
+  echo "${plain_file}"
+done
