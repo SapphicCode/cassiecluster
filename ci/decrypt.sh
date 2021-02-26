cf8bb2f7a977568e7ae2c48eb277f485326c6c0f
Cassandra <cassandra+git@beelen.one>
2021-02-26 18:17:04 +0100

    Fix broken decryption script

--- ci/decrypt.sh
+++ ci/decrypt.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 
 for encrypted_file in **/*.transit; do
   plain_file=${encrypted_file%.*}
-  vault write -field=plaintext transit/cluster/decrypt/automata "ciphertext=@${encrypted_file}" | base64 -d > "${plain_file}" || rm "${plain_file}" && false
+  vault write -field=plaintext transit/cluster/decrypt/automata "ciphertext=@${encrypted_file}" | base64 -d > "${plain_file}"
   touch -r "${encrypted_file}" "${plain_file}"
   echo "${plain_file}"
 done
