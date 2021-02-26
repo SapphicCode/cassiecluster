7f2c8390a198c718b402797d904a33416bc910e0
Cassandra <cassandra+git@beelen.one>
2021-02-26 23:27:23 +0100

    Add Concourse CI tooling

--- ci/decrypt.sh
+++ ci/decrypt.sh
@@ -3,6 +3,9 @@
 shopt -s globstar
 set -euxo pipefail
 
+VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="${VAULT_APPROLE_ROLE}" secret_id "${VAULT_APPROLE_SECRET}")
+export VAULT_TOKEN
+
 for encrypted_file in **/*.transit; do
   plain_file=${encrypted_file%.*}
   vault write -field=plaintext transit/cluster/decrypt/automata "ciphertext=@${encrypted_file}" | base64 -d > "${plain_file}"
