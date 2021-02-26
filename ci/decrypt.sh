6e210b46aa682fc56f5d6394078272c0e2950673
Cassandra <cassandra+git@beelen.one>
2021-02-26 19:02:28 +0100

    Add chmod support to the decryption script

--- ci/decrypt.sh
+++ ci/decrypt.sh
@@ -6,6 +6,7 @@ set -euxo pipefail
 for encrypted_file in **/*.transit; do
   plain_file=${encrypted_file%.*}
   vault write -field=plaintext transit/cluster/decrypt/automata "ciphertext=@${encrypted_file}" | base64 -d > "${plain_file}"
-  touch -r "${encrypted_file}" "${plain_file}"
+  touch --reference="${encrypted_file}" "${plain_file}"
+  chmod --reference="${encrypted_file}" "${plain_file}"
   echo "${plain_file}"
 done
